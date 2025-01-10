3b355c722a35c3bf6801a4411f9f5fd95c0ca99d
Nachiket Bidarkundi <87447470+nachobid08@users.noreply.github.com>
2025-01-10 22:46:55 +0000

    new changes

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        
+        }
+
+        System.out.println("The sum of all multiples of 3 or 5 below 1000 is: ");
 
 
-
-
+    }
 }
